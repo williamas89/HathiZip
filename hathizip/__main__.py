5306467317246314554e1ceb685c2593c206a584
hborcher <hborcher@illinois.edu>
2017-07-20 16:57:48 -0500

    added pytest from command line

--- hathizip/__main__.py
+++ hathizip/__main__.py
@@ -1,8 +1,14 @@
+import sys
+
 from hathizip import cli
 
 
 def main():
-    cli.main()
+    if len(sys.argv) > 1 and sys.argv[1] == "--pytest":
+        import pytest  # type: ignore
+        sys.exit(pytest.main(sys.argv[2:]))
+    else:
+        cli.main()
 
 
 if __name__ == '__main__':
