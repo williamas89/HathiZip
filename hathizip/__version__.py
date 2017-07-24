698499518d19c1e766a746a1766be538e813585f
hborcher <hborcher@illinois.edu>
2017-07-24 15:57:01 -0500

    fix URL

--- hathizip/__version__.py
+++ hathizip/__version__.py
@@ -1,6 +1,6 @@
 __title__ = 'HathiZip'
 __version__ = '0.0.1'
-__url__ = 'https://github.com/UIUCLibrary/HathiValidate'
+__url__ = 'https://github.com/UIUCLibrary/HathiZip'
 __author__ = 'University of Illinois at Urbana Champaign'
 __author_email__ = 'hborcher@illinois.edu'
 __description__ = 'Creates .zip file packages for HathiTrust. Replacement script for HathiTrust Zip and Submit.'
